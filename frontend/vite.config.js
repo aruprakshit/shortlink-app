48dd3f059fc50376d68d5da3400a796dd83ebaf3
aruprakshit <aruprakshit@rocketmail.com>
2025-09-16 01:54:30 +0530

    404 debugging config.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'http://192.168.49.2',
         changeOrigin: true,
       },
     },
